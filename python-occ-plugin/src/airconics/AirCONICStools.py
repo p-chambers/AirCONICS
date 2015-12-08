b2e7c35b171f5abe31355efaf2266277ab49d13a
Paul Chambers <P.R.Chambers@soton.ac.uk>
2015-12-08 17:03:58 +0000

    Added ipython notebook gui. Updated airfoil definition and primitives

--- python-occ-plugin/src/airconics/AirCONICStools.py
+++ python-occ-plugin/src/airconics/AirCONICStools.py
@@ -6,6 +6,7 @@ Created on Fri Dec  4 11:58:52 2015
 """
 from OCC.BRepBndLib import brepbndlib_Add
 from OCC.Bnd import Bnd_Box
+from OCC.TColgp import TColgp_Array1OfPnt2d
 
 
 def ObjectsExtents(ObjectIds, tol=1e-6, as_vec=False):
@@ -37,4 +38,20 @@ def ObjectsExtents(ObjectIds, tol=1e-6, as_vec=False):
         return xmin, ymin, zmin, xmax, ymax, zmax
     else:
         return gp_Vec(xmin, ymin, zmin), gp_Vec(xmax, ymax, zmax)
-    
+
+
+def _Tcol_dim_1(li, _type):
+    """
+    Function factory for 1-dimensional TCol* types
+    """
+    pts = _type(0, len(li)-1)
+    for n, i in enumerate(li):
+        pts.SetValue(n, i)
+    return pts
+
+
+def point2d_list_to_TColgp_Array1OfPnt2d(li):
+    """
+    List of gp_Pnt2d to TColgp_Array1OfPnt2d
+    """
+    return _Tcol_dim_1(li, TColgp_Array1OfPnt2d)  
--- python-occ-plugin/src/setup.py
+++ python-occ-plugin/src/setup.py
@@ -6,16 +6,16 @@ Created on Fri Dec  4 16:13:40 2015
 """
 
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 setup(
     name='airconics',
     version="0.21",
     description='Scripted Aircraft Geometry Module based on Python-OCC',
-    packages=['airconics'],
-    include_package_data=True,
-    package_data={'airconics': ['coord-seligFmt/*.dat']},
+    packages=find_packages(),
+#    include_package_data=True,
+    package_data={'coord_seligFmt': ['*.dat']},
     author='Andras Sobester, Paul Chambers',
     author_email='A.Sobester@soton.ac.uk, P.R.Chambers@soton.ac.uk'
 )
